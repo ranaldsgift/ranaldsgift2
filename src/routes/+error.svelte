82922fef070e3973f109258cce3077c998ebe789
ranaldsgift <kevinjohncampbell@gmail.com>
2024-07-24 18:31:04 -0400

    Updated error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,8 @@
 
 <svelte:head><title>Holy Sigmar! - augs.tools</title></svelte:head>
 
-<div id="page" class="text-white grid">
+<div id="page" class="text-white flex flex-col mx-auto top-10 relative">
+	<span class="title">Ranald's Gift</span>
 	<span class="divider-20 h-[59px]"></span>
 	<div class="error">
 		<h2 class="text-2xl mt-4">Holy Sigmar!</h2>
@@ -19,12 +20,13 @@
 		width: fit-content;
 		max-width: 1200px;
 		margin: 0 auto;
+		z-index: 1;
 	}
 	.error {
 		margin: 0 auto;
 		width: calc(100% - 20px);
-		background: rgba(0, 0, 0, 0.2);
-		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+		background: rgba(0, 0, 0, 0.3);
+		box-shadow: 0 4px 30px rgba(0, 0, 0, 0.2);
 		backdrop-filter: blur(5px);
 		-webkit-backdrop-filter: blur(5px);
 		text-align: center;
@@ -48,5 +50,16 @@
 			max-height: 1000px;
 			display: block;
 		}
+	}
+
+	.title {
+		text-transform: uppercase;
+		position: relative;
+		top: 0;
+		color: #c15b24;
+		font-size: 3em;
+		letter-spacing: 5px;
+		text-shadow: 1px 1px #000;
+		margin: 0 auto;
 	}
 </style>
