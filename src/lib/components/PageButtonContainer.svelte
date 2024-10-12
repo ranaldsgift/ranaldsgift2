750e1244d86405ae73943379362773245c147d37
HighImKevin <kevinjohncampbell@gmail.com>
2024-10-12 16:08:10 -0400

    Update Page Button Container UI

--- src/lib/components/PageButtonContainer.svelte
+++ src/lib/components/PageButtonContainer.svelte
@@ -8,6 +8,13 @@
     const { children }: Props = $props();
 </script>
 
-<div class="tablet:fixed z-[10001] tablet:top-[28px] top-0 flex gap-5 mb-2 tablet:ml-[10px] ml-0 tablet:mr-0 mr-4 tablet:pt-0 pt-2 sticky tablet:mb-0 tablet:justify-start justify-end">
+<div class="tablet:fixed z-[10001] tablet:top-[25px] top-0 flex gap-5 mb-2 tablet:ml-[10px] ml-0 tablet:mr-0 mr-4 tablet:pt-0 pt-2 sticky tablet:mb-0 tablet:justify-start justify-end">
     {@render children()}
 </div>
+
+<style>
+	div > :global(a) {
+        text-decoration: none;
+	}
+</style>
+
